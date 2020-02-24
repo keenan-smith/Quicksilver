6f3955978f92e4eac8521dadf49e8beb609143a1
Keenan Smith <keenan.smith500@gmail.com>
2020-02-24 02:33:32 -0500

    cleaned driverentry, (dont create driver, thats a detection vector)

--- POC/RinglandDriver/main.c
+++ POC/RinglandDriver/main.c
@@ -1,25 +1,10 @@
 #include "stdafx.h"
 #include "ntos.h"
 
-#define drv L"\\Driver\\ring0inj"
-
-PDEVICE_OBJECT driver_object;
-UNICODE_STRING dev, dos;
-
-NTSTATUS initialize(PDRIVER_OBJECT driver, PUNICODE_STRING path) {
-	UNREFERENCED_PARAMETER(driver);
-	UNREFERENCED_PARAMETER(path);
-
-	DbgPrint("Test from DriverObject, DriverName = %wZ", &driver->DriverName);
-	return STATUS_SUCCESS;
-}
-
 NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING registryPath) {
 	UNREFERENCED_PARAMETER(driver);
 	UNREFERENCED_PARAMETER(registryPath);
 
-	UNICODE_STRING drv_name;
-	RtlInitUnicodeString(&drv_name, drv);
-	DbgPrint("Test from DriverEntry");
-	return IoCreateDriver(&drv_name, &initialize);
+	DbgPrintEx(0, 0, "Test from DriverEntry");
+	return 0xDEAD10CC;
 }
