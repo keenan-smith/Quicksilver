1b5ae38e1f174c2e3925ea699cfcc881510a6d7e
Keenan Smith <keenan.smith500@gmail.com>
2020-02-24 20:39:25 -0500

    added berkeley sockets

--- POC/RinglandDriver/main.cpp
+++ POC/RinglandDriver/main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "ntos.h"
+#include "sockets.h"
+#include "log.h"
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING registryPath) {
 	UNREFERENCED_PARAMETER(driver);
