c61f86d5ce55f65b2d0ad4f799317054a7e06ab5
Keenan Smith <keenan.smith500@gmail.com>
2020-03-01 17:51:13 -0500

    integrated kernel virtualprotect+virtualalloc with usermode API and controller

--- POC/MMap/usermode_proc_handler.cpp
+++ POC/MMap/usermode_proc_handler.cpp
@@ -7,7 +7,7 @@ usermode_proc_handler::usermode_proc_handler()
 
 usermode_proc_handler::~usermode_proc_handler() { if (handle) CloseHandle(handle); }
 
-bool usermode_proc_handler::is_attached() { return handle; }
+bool usermode_proc_handler::is_attached() { return pid; }
 
 bool usermode_proc_handler::attach(const char* proc_name) {
 	while (!is_process_running(proc_name, pid))
@@ -15,7 +15,7 @@ bool usermode_proc_handler::attach(const char* proc_name) {
 
 	//handle = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, pid);
 
-	return 0;
+	return 1;
 }
 
 uint64_t usermode_proc_handler::get_module_base(const std::string& module_name) {
@@ -59,11 +59,14 @@ void usermode_proc_handler::create_thread(uintptr_t start, uintptr_t arg) {
 }
 
 uint32_t usermode_proc_handler::virtual_protect(uint64_t address, size_t size, uint32_t protect) { //who cares about old protect anyways hahahahhahahahaaaaaa
-	DWORD old_protect{};
-	VirtualProtectEx(handle, (LPVOID)address, size, protect, &old_protect);
+	DWORD old_protect{0};
+	if (driver::virtual_protect(sConnection, pid, address, size, protect) != 0)
+		LOG("Error in virtual_protect!");
+	//VirtualProtectEx(handle, (LPVOID)address, size, protect, &old_protect);
 	return old_protect;
 }
 
 uint64_t usermode_proc_handler::virtual_alloc(size_t size, uint32_t allocation_type, uint32_t protect, uint64_t address) {
-	return (uint64_t)VirtualAllocEx(handle, (void*)address, size, allocation_type, protect);
+	return driver::virtual_alloc(sConnection, pid, size, allocation_type, protect, address);
+	//return (uint64_t)VirtualAllocEx(handle, (void*)address, size, allocation_type, protect);
 }
