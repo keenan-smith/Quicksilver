3917e47ebec1f6e4de681a146a378b0f7d79f130
hiilikewiki56 <archiestride42@gmail.com>
2020-10-18 14:05:53 +0100

    paste from mine
    Visual studio version control is bad so using web editor LMAO

--- POC/RinglandDriver/main.cpp
+++ POC/RinglandDriver/main.cpp
@@ -27,7 +27,25 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING registryPath) {
 		log("Unable to create server thread. Status code: 0x%X.", status);
 		return 0xDEAD10CC;
 	}
+	
+	status = ObReferenceObjectByHandle(thread_handle, THREAD_ALL_ACCESS, NULL, KernelMode, (PVOID*)&thread, NULL);
+
+	if(!NT_SUCCESS(status)) {
+		log("Unable to create server thread. Status code: 0x%X.", status);
+		return status;
+	}
+	
+	driver->DriverUnload = DriverUnload;
 
 	ZwClose(thread_handle);
-	return 0x0;
+	return STATUS_SUCCESS;
+}
+
+VOID DriverUnload(IN PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	
+	shut_down_server = TRUE;
+	KeWaitForSingleObject(thread, Executive, KernelMode, FALSE, NULL);
+	ObDereferenceObject(thread);
 }
